7ed2554ab01ecc01d118d16340d013c2e42b3425
Phelipe Magalhães <phelipeamagalhaes@gmail.com>
2021-12-22 11:55:51 -0300

    implementa typeorm e migrations de Category

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM node:12-alpine
+FROM node
 
 WORKDIR /usr/app
 
 COPY package.json ./
 
-RUN npm install
+RUN npm i --force
 
 COPY . .
 
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 import { router } from "./routes";
+import "./database";
 
 const app = express();
 
