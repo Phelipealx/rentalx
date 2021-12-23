ec5ab78b49d76ad11dcfd8c191ab447d81821d42
phelipe.alex <phelipe.alex@totvs.com.br>
2021-12-23 11:20:46 -0300

    refatora category repository

--- src/modules/cars/useCases/createCategory/CreateCategoryController.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.ts
@@ -4,8 +4,8 @@ import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
 
 class CreateCategoryController {
     constructor(private createCategoryUseCase: CreateCategoryUseCase) {}
-    handle(request: Request, response: Response): Response {
-        this.createCategoryUseCase.execute(request.body);
+    async handle(request: Request, response: Response): Promise<Response> {
+        await this.createCategoryUseCase.execute(request.body);
 
         return response.status(201).send();
     }
